7adc7fe28048e3b07f7a890ef78b1c0e4419a37f
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-07-30 14:17:28 -0300

    Add Spring Boot

---
+++ src/main/java/com/raphael/cardealership/CarDealerShipApplication.java
@@ -0,0 +1,13 @@
+package com.raphael.cardealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealerShipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarDealerShipApplication.class, args);
+    }
+
+}
