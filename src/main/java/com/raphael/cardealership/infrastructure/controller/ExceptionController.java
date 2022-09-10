ac20983de19bba1c0504a302e0495ea8ce0cd02d
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-09-10 14:09:41 -0300

    Create Exception Handlers

---
+++ src/main/java/com/raphael/cardealership/infrastructure/controller/ExceptionController.java
@@ -0,0 +1,52 @@
+package com.raphael.cardealership.infrastructure.controller;
+
+import com.raphael.cardealership.domain.shared.EntityNotFoundException;
+import com.raphael.cardealership.domain.shared.EntityValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionController {
+    private static final String GENERIC_SERVER_ERROR_BODY_MESSAGE = "There's been an unexpected error. Please, check logs or contact support.";
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionBody> handleEntityNotFoundException(EntityNotFoundException e) {
+        log.error("An attempt to access an unknown resource was made: {}", e.getMessage(), e);
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ExceptionBody.fromMessage(e.getMessage()));
+    }
+
+    @ExceptionHandler(EntityValidationException.class)
+    public ResponseEntity<ExceptionBody> handleEntityValidationException(EntityValidationException e) {
+        log.error("An attempt to modify or create a resource with invalid data was made: {}", e.getMessage(), e);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ExceptionBody.fromMessage(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionBody> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("An attempt to modify or create a resource with malformed data was made: {}", e.getMessage(), e);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ExceptionBody.fromException(e));
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<ExceptionBody> handleGeneralError(Throwable throwable) {
+        log.error("An unexpected error was thrown when performing the request: {}", throwable.getMessage(), throwable);
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ExceptionBody.fromMessage(GENERIC_SERVER_ERROR_BODY_MESSAGE));
+    }
+}
