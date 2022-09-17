f46ebcf1958416a77e613bc1be7f622b0b9c40c5
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-09-17 17:37:29 -0300

    Enable Cors

--- src/main/java/com/raphael/cardealership/infrastructure/config/WebSecurityConfig.java
+++ src/main/java/com/raphael/cardealership/infrastructure/config/WebSecurityConfig.java
@@ -3,11 +3,18 @@ package com.raphael.cardealership.infrastructure.config;
 import com.raphael.cardealership.infrastructure.controller.filter.FilterChainExceptionHandler;
 import com.raphael.cardealership.infrastructure.controller.filter.JWTAuthorizationFilter;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @AllArgsConstructor
@@ -18,10 +25,23 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
                 .addFilterBefore(filterChainExceptionHandler, LogoutFilter.class)
                 .addFilterAfter(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/v3/api-docs/**", "/docs/**", "/swagger-ui/**", "/auth/**", "/participant/**").permitAll()
                 .anyRequest().authenticated();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
